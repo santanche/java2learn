8a4eca96d304a5624ad582cb805a4076e2ab44a3
santanche <santanche@tapejara>
2018-06-30 12:46:33 -0300

    Final.

--- src/pt/c08componentes/s20catalog/s10ds/IDataSet.java
+++ src/pt/c08componentes/s20catalog/s10ds/IDataSet.java
@@ -2,7 +2,5 @@ package pt.c08componentes.s20catalog.s10ds;
 
 import pt.c08componentes.s20catalog.s00shared.IInstancesProducer;
 
-public interface IDataSet extends IInstancesProducer {
-  public String getDataSource();
-  public void setDataSource(String dataSource);
+public interface IDataSet extends IInstancesProducer, IDataSource {
 }
