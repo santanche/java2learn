1648b13671781a0a275fd446031c9b85a4554125
Andre Santanche <santanche@gmail.com>
2018-08-10 23:20:09 -0300

    Machine Learning component.

---
+++ src/pt/c08componentes/s20catalog/apps/App90aDataSetDiagnostics.java
@@ -0,0 +1,24 @@
+package pt.c08componentes.s20catalog.apps;
+
+import pt.c08componentes.s20catalog.s10ds.DataSetComponent;
+import pt.c08componentes.s20catalog.s10ds.IDataSet;
+import pt.c08componentes.s20catalog.s90ml.DiagnosticsComponent;
+import pt.c08componentes.s20catalog.s90ml.IDiagnostics;
+
+public class App90aDataSetDiagnostics {
+  public static void main(String args[])
+  {
+	  double[] symptoms = {0, 0, 1, 1, 1, 1, 1, 0};
+      try {
+        IDataSet dataset = new DataSetComponent();
+        dataset.setDataSource("db/datasets/zombie/weka/zombie-health-spreadsheet-ml-training.arff");
+        
+        IDiagnostics diagnostics = new DiagnosticsComponent();
+        diagnostics.connect(dataset);
+        
+        diagnostics.update(symptoms);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+  }
+}
---
+++ src/pt/c08componentes/s20catalog/s90ml/IDiagnose.java
@@ -0,0 +1,5 @@
+package pt.c08componentes.s20catalog.s90ml;
+
+public interface IDiagnose {
+  public void update(double[] symptoms);
+}
