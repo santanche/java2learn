7b549e6cb19b7adeccd2d16d29663f6908fcb64e
André Santanchè <santanche@gmail.com>
2019-06-04 09:25:33 -0300

    Exercícios Patterns e Anotações

---
+++ eclipse/src/pt/c03java/s00package/s01classe/AnunciaDinossauro.java
@@ -0,0 +1,7 @@
+package pt.c03java.s00package.s01classe;
+
+public class AnunciaDinossauro {
+  public void anuncia() {
+    System.out.println("O dinossauro pulou na lama");
+  }
+}
---
+++ eclipse/src/pt/c03java/s00package/s02principal/AppAnuncia.java
@@ -0,0 +1,12 @@
+package pt.c03java.s00package.s02principal;
+
+import pt.c03java.s00package.s01classe.AnunciaDinossauro;
+
+public class AppAnuncia {
+
+  public static void main(String[] args) {
+    AnunciaDinossauro dino = new AnunciaDinossauro();
+    dino.anuncia();
+  }
+
+}
